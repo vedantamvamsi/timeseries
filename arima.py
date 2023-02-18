a86864bcf549f090cbb0c83214d6991c637add78
Vamsi <vedantamvamsi@gmail.com>
2023-02-18 22:44:35 +0100

    Update arima.py

--- arima.py
+++ arima.py
@@ -1,13 +1,33 @@
 import pandas as pd
 from statsmodels.tsa.arima.model import ARIMA
 
-# Load time series data into a Pandas DataFrame
-data = pd.read_csv('time_series_data.csv', index_col='Date', parse_dates=True)
+# Load time series data into a DataFrame in Pandas
+data = pd.read_csv('sales.csv', index_col='Date', parse_dates=True)
+
+### check for stationarity using Dickey-Fuller test ### 
+
+plt.plot(data)
+plt.title('time series distribution on sales data')
+plt.xlabel('time')
+plt.ylabel('value')
+plt.show()
+
+result = adfuller(data['Value'])
+
+print('ADF Statistic:', result[0])
+print('p-value:', result[1])
+
+for key, value in result[4].items():
+    print('Critical Value at %s: %.3f' % (key, value))
+
+### The test returns a test statistic and a p-value, and we can compare the test statistic to critical values at different significance levels to determine whether the null hypothesis can be rejected or not. 
+### If the p-value is less than a chosen significance level (such as 0.05), we can reject the null hypothesis and conclude that the data is stationary.
+
 
 # Split the data into training and testing sets
 train_data = data.iloc[:-12]
 test_data = data.iloc[-12:]
-
+    
 # Create an ARIMA model with (p,d,q) parameters
 model = ARIMA(train_data, order=(2,1,2))
 
